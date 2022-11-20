b941915f13a24800b939d2b9d9a617863e738f4e
Hewrxque <josehenriquevieiradacruz@gmail.com>
2022-11-20 11:33:56 -0300

    first commit

---
+++ src/config/database.js
@@ -0,0 +1,13 @@
+module.exports= {
+    dialect: 'mysql',
+    host: 'localhost',
+    username: 'root',
+    password: 'root',
+    database: 'apiClinicas',
+    port: '3306',
+   
+    define: {
+        timeStamps: true,
+        underscored: true,
+    }
+}
